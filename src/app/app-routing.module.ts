c9689342d348f3c3facf7afed97c502e57277359
kehud <ehudkakun@hotmail.com>
2023-10-25 10:28:49 +0300

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/pages/home/home.component';
+import { CardListComponent } from './core/pages/card-list/card-list.component';
+import { VideoListComponent } from './core/pages/video-list/video-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  "", pathMatch:  "full",redirectTo:  "home"},
+  {path: "home", component: HomeComponent},
+  {path: "card-list", component: CardListComponent},
+  {path: "video-list", component: VideoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/pages/home/home.component';
+import { CardListComponent } from './core/pages/card-list/card-list.component';
+import { VideoListComponent } from './core/pages/video-list/video-list.component';
+import { HeaderComponent } from './common/components/header/header.component';
+import { FooterComponent } from './common/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardListComponent,
+    VideoListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
